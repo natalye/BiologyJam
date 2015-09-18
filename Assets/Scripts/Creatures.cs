660af14db0d5b4f462605437a6edd459f217a424
amitmuzi <amitmuzi@walla.co.il>
2015-09-18 19:15:32 +0300

    Creatures Classes

---
+++ Assets/Scripts/Creatures.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Creatures : MonoBehaviour {
+
+	public class Bacteria{
+		int Hp;
+		int Speed;
+		int BreedingMeter=0;
+	}
+	
+	public class Phagocyte{
+		string Type;
+		int Hp;
+		int Dmg;
+		int SwallowSpeed;
+		int Speed;
+	}
+	
+	public class BreedingArea{
+		int Food;
+	}
+
+}
