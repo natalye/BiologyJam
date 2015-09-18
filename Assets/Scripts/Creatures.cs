0367b96d1da3b8faebbc932e35a242d6b6d30cb3
amitmuzi <amitmuzi@walla.co.il>
2015-09-18 21:31:26 +0300

    Prefabs

--- Assets/Scripts/Creatures.cs
+++ Assets/Scripts/Creatures.cs
@@ -4,21 +4,27 @@ using System.Collections;
 public class Creatures : MonoBehaviour {
 
 	public class Bacteria{
-		int Hp;
-		int Speed;
-		int BreedingMeter=0;
+		public int Hp { get; set; }
+		public	int Speed { get; set; }
+		public int BreedingMeter { get; set; }
+		 
+		public Bacteria(int hp, int speed){
+			Speed=speed;
+			Hp=hp;
+			BreedingMeter=0;
+		}
 	}
 	
 	public class Phagocyte{
-		string Type;
-		int Hp;
-		int Dmg;
-		int SwallowSpeed;
-		int Speed;
+		string Type { get; set; }
+		int Hp { get; set; }
+		int Dmg { get; set; }
+		int SwallowSpeed { get; set; }
+		int Speed { get; set; }
 	}
 	
-	public class BreedingArea{
-		int Food;
+	public class ReproductionArea{
+		int Food { get; set; }
 	}
 
 }
---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Creatures.Bacteria test = new Creatures.Bacteria (5, 30);
+		Debug.Log (test.Hp + "" + test.Speed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
