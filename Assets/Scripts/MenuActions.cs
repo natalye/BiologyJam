6f3600aabe020382249f2a96603b5085a4d7d5e8
idoadler <ido.adler@mail.huji.ac.il>
2015-09-19 17:04:37 +0300

    Main Menu

---
+++ Assets/Scripts/MenuActions.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEditor;
+
+public class MenuActions : MonoBehaviour {
+    public string backScene;
+
+    public void Play()
+    {
+        Application.LoadLevel("Game");
+    }
+    public void About()
+    {
+        Application.LoadLevel("About");
+    }
+    public void Menu()
+    {
+        Application.LoadLevel("Menu");
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            if (string.IsNullOrEmpty(backScene))
+                Exit();
+            else
+                Application.LoadLevel(backScene);
+        }
+    }
+}
